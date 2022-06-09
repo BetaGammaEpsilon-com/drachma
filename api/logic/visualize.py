296ec3e926c2023a68fbc62c55b098aa9e31c719
Robert Chatterton <robbie.chatterton@gmail.com>
2022-06-09 16:25:18 -0400

    basic setup

---
+++ api/logic/visualize.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+
+
+def logic_visualize_transaction(verified, unverified):
+
+    right = datetime.today()
+    left = datetime.today() - timedelta(days=30)
+
+    fig = plt.figure()
+    
+
+
+
+if __name__ == '__main__':
+    pass
